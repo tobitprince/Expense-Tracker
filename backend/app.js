dd76bd423d11ac5414d7bb0a38e1564cbfb95285
tobitprince <prince.tobit@strathmore.edu>
2024-02-01 23:04:31 +0300

    Refactor variable names for clarity

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+const server= () => {
+    app.listen(PORT, () =>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+
+}
+server()
